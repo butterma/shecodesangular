a9b14296b5fe2b0ce1b175cd71839b51c269810d
butterma <butterma@g.jct.ac.il>
2018-09-20 13:17:57 +0300

    k

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup,FormBuilder,Validators} from '@angular/forms';
+import {AlertService} from '../services/alert.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,13 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm:FormGroup;
+
+  constructor(private fb:FormBuilder,private alertService:AlertService) {
+    this.contactForm=this.fb.group({
+      name:'',
+      email:'',
+      message:''
+    });
+   }
 
   ngOnInit() {
   }
 
   sendContact(name,email,message){
-    
+    this.contactForm.get('name').setValue("");
+    this.contactForm.get('email').setValue("");
+    this.contactForm.get('message').setValue("");
+    this.alertService.success('Thanks for your message, keep in touch :)');
   }
 
 }
